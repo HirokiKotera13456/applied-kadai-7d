85f84d9ed5feada7299b83269f4d3d1081a6f78f
nijimajohn <hirokikotera0211@gmail.com>
2021-07-17 02:09:27 +0000

    [mid]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,34 @@ class BooksController < ApplicationController
 
 
   def create
-    @user = current_user
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+    @books = Book.all
+    @book=Book.new(book_params)
+    @book.user_id= current_user.id
+   if @book.save
+     redirect_to book_path(@book.id),notice: "You have created book successfully."
+   else
+       @user = current_user
+       render :index
+   end
   end
 
   def index
     @book=Book.new
     @books=Book.all
+    @book.user_id = current_user.id
+  end
+  
+  def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+    @book=Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "You have updated book successfully."
+   else
+    render :edit
+   end
   end
 
   def show
@@ -21,7 +38,6 @@ class BooksController < ApplicationController
 
   def destroy
     @books = Book.find(params[:id])
-    flash[:notice] = "Book was successfully destroyed."
     @books.destroy
     redirect_to books_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy, :update]
+  get "home/about" => "homes#about" 
+  resources :books, only: [:edit, :create, :index, :show, :destroy, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 end
