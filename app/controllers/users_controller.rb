a5bacc91ae2904baedfab3f51bac1314dcbd8269
nijimajohn <hirokikotera0211@gmail.com>
2021-08-04 11:06:29 +0000

    [finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
     @book=Book.new
+    @user = User.find(params[:id])
     @books=@user.books
   end
 
