c95a672eca4dd637fc118cb2017bef3b3787bf48
nijimajohn <hirokikotera0211@gmail.com>
2021-08-04 08:42:15 +0000

    [finish

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about" 
   resources :books, only: [:edit, :create, :index, :show, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update, :index]
